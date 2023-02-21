dedadf0c3f6800b2b3d2b9a8dd27a76b58b2b9e1
cheneg733 <1170725964@qq.com>
2023-02-21 22:47:31 +0800

    Revert "feat: 跨主题左右上下移动"
    This reverts commit 16db671ac9b72fe8b50905602ce2a994d631f9fb.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
-import { Container } from './Container';
-/**
- * 
- * 需求：
- * 同一个主题：
- * 上下左右互换
- * 不同主题：
- * 相当于插入一个新的课程
- * 当到一个主题时打开这个主题  -- 还没实现
- * 
- */
 
 function App() {
-
   return (
     <div className="App">
-      <DndProvider backend={HTML5Backend}>
-     <Container/>
-     </DndProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-   
   );
 }
-
-
-
-
-
-
-
 
 export default App;
